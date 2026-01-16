bbfdec9b7b5b91449dd2739a45e89c857d2c15c7
lxxx <1318753541@qq.com>
2026-01-16 20:23:46 +0800

    初始化项目

---
+++ src/main/java/com/zorroe/cloud/filebox/FileBoxApplication.java
@@ -0,0 +1,13 @@
+package com.zorroe.cloud.filebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileBoxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileBoxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zorroe/cloud/filebox/FileBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorroe.cloud.filebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBoxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
