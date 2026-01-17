3d87a8682b2d353c304e7eb0240e751da88a7c3c
lxxx <1318753541@qq.com>
2026-01-17 10:26:37 +0800

    config(database): 配置MySQL数据源连接参数
    - 移除Admin实体类中的DateTimeFormat注解
    - 添加Spring Boot数据源配置
    - 配置MySQL数据库连接URL、用户名和密码
    - 设置HikariCP连接池参数包括最大连接数、最小空闲连接等
    - 配置数据库驱动类名和连接超时时间

--- src/main/java/com/zorroe/cloud/filebox/entity/Admin.java
+++ src/main/java/com/zorroe/cloud/filebox/entity/Admin.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
